b440a4e1a711c89183fbe617d25f10ddedca05cb
Anas <mkhairulanas1st@gmail.com>
2023-11-07 14:02:50 +0700

    updated

--- app/components/MoviesCard.tsx
+++ app/components/MoviesCard.tsx
@@ -48,7 +48,7 @@ const MoviesCard: React.FC<MovieCardProps> = ({ data }) => {
               onClick={() => {
                 openModal(data?.id);
               }}
-              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
+              className="cursor-pointer ml-auto  w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
             >
               <BiChevronDown
                 size={20}
--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,10 +1,9 @@
-import { NextRequest } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./utils";
 
 import prismadb from "@/lib/prismadb";
 
-const serverAuth = async (request: NextRequest) => {
+const serverAuth = async () => {
   const session = await getServerSession(authOptions);
   if (!session?.user?.email) {
     throw new Error("Not signed in");
